e34bfc6b110df3ee076381ac6f34bbf8297d2fcf
Denys Smirnov <dennwc@pm.me>
2023-11-23 14:09:50 +0200

    Update copyright in all files.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// 	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,6 +26,7 @@ import (
 	"github.com/livekit/protocol/utils"
 	"github.com/livekit/psrpc"
 	lksdk "github.com/livekit/server-sdk-go"
+
 	"github.com/livekit/sip/pkg/errors"
 )
 
