bdb45ea5d2f66086285d0c9bbe74c21e0eae42e6
Denys Smirnov <dennwc@pm.me>
2024-03-22 16:00:23 +0200

    Give clients more time in the test to check audio.

--- test/integration/sip_test.go
+++ test/integration/sip_test.go
@@ -436,7 +436,7 @@ func TestSIPAudio(t *testing.T) {
 			}
 			lk.ExpectRoomWithParticipants(t, ctx, roomName, exp)
 
-			ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+			ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
 			defer cancel()
 			CheckAudioForParticipants(t, ctx, audios...)
 			cancel()
