ff9dca04932b086e6e4fcda3ba88b9f6a21f61b3
ftsef <131373548+ftsef@users.noreply.github.com>
2024-06-19 14:08:27 +0200

    fix BYE 481 Call/Transaction Does Not Exist

--- pkg/sip/server.go
+++ pkg/sip/server.go
@@ -218,7 +218,19 @@ func (s *Server) Start(agent *sipgo.UserAgent, unhandled sipgo.RequestHandler) e
 	s.sipUnhandled = unhandled
 
 	// Ignore ACKs
-	s.sipSrv.OnAck(func(req *sip.Request, tx sip.ServerTransaction) {})
+	s.sipSrv.OnAck(func(req *sip.Request, tx sip.ServerTransaction) {
+	        fromTag, err := getTagValue(req)
+	        if err != nil {
+	            sipErrorResponse(tx, req)
+	            return
+	        }
+	
+	        if c, found := s.activeCalls[fromTag]; found {
+	            if header, ok := req.To(); ok {
+	                c.to.Params["tag"] = header.Params["tag"]
+	            }
+	        }
+    	})
 
 	if err := s.startUDP(); err != nil {
 		return err
