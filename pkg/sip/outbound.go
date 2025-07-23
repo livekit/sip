71966230cf270d5615ecb9dfe939308efb8a9dff
Denys Smirnov <dennwc@pm.me>
2025-07-23 12:15:47 +0000

    Propagate original SIP response status text. (#421)

--- pkg/sip/outbound.go
+++ pkg/sip/outbound.go
@@ -777,13 +777,19 @@ authLoop:
 		case sip.StatusOK:
 			break authLoop
 		default:
-			return nil, fmt.Errorf("unexpected status from INVITE response: %w", &livekit.SIPStatus{Code: livekit.SIPStatusCode(resp.StatusCode)})
+			return nil, fmt.Errorf("unexpected status from INVITE response: %w", &livekit.SIPStatus{
+				Code:   livekit.SIPStatusCode(resp.StatusCode),
+				Status: resp.Reason,
+			})
 		case sip.StatusBadRequest,
 			sip.StatusNotFound,
 			sip.StatusTemporarilyUnavailable,
 			sip.StatusNotAcceptableHere,
 			sip.StatusBusyHere:
-			err := &livekit.SIPStatus{Code: livekit.SIPStatusCode(resp.StatusCode)}
+			err := &livekit.SIPStatus{
+				Code:   livekit.SIPStatusCode(resp.StatusCode),
+				Status: resp.Reason,
+			}
 			if body := resp.Body(); len(body) != 0 {
 				err.Status = string(body)
 			} else if s := resp.GetHeader("X-Twillio-Error"); s != nil {
