4c20e8cf7464d31fc782bdb8e67e8d9ff40d19da
Alex <alex.migolin@livekit.io>
2026-08-13 13:06:46 -0700

    TEL-886: Disable pipeline rebuild, allow multi-DTMF (#791)

--- pkg/sip/media_pipeline.go
+++ pkg/sip/media_pipeline.go
@@ -287,6 +287,7 @@ func (p *mediaPortPipeline) setupOutput(mc *sdp.MediaConfig, incomingSampleRate 
 		}
 
 		p.dtmfToPort = &dtmfOutWriter{
+			log:          p.conf.log,
 			ctx:          p.ctx,
 			dtmfEvents:   s.NewStream(mc.Audio.DTMFType, dtmf.SampleRate),
 			dtmfAudio:    dtmfAudio,
@@ -410,6 +411,8 @@ func (p *mediaPortPipeline) Close() error {
 
 // dtmfOutWriter sends SipDTMF as RFC 4733 telephone-events (optional in-band audio).
 type dtmfOutWriter struct {
+	log logger.Logger
+
 	mu           sync.Mutex
 	ctx          context.Context // canceled by pipeline Close, aborts an in-flight digit train
 	dtmfEvents   *rtp.Stream
@@ -433,25 +436,30 @@ func (w *dtmfOutWriter) Close() error {
 }
 
 func (w *dtmfOutWriter) WriteSample(sample *livekit.SipDTMF) error {
-	if sample == nil || len(sample.Digit) > 1 || sample.Code >= 0x10 {
+	if sample == nil || sample.Code >= 0x10 || (len(sample.Digit) == 0 && sample.Code == 0) {
 		return fmt.Errorf("invalid DTMF sample: %v", sample)
 	}
-	digit := byte(0)
-	if len(sample.Digit) == 1 {
-		digit = sample.Digit[0]
-	} else {
-		digit = dtmf.CodeToChar(byte(sample.Code))
-	}
-	if digit == 0 {
-		return fmt.Errorf("invalid DTMF sample: %v", sample)
+	digits := sample.Digit
+	if len(digits) == 0 {
+		digit := dtmf.CodeToChar(byte(sample.Code))
+		if digit == 0 {
+			return fmt.Errorf("code %d not supoported", sample.Code)
+		}
+		digits = string([]byte{digit})
+	} else if sample.Code > 0 {
+		// We can't distinguish between a code0 and no code, but better have something here
+		w.log.Warnw("code payload detected, ignored due to explicit digits", nil, "code", sample.Code, "digits", sample.Digit)
 	}
 
 	w.mu.Lock()
 	defer w.mu.Unlock()
-
 	var rtpTs uint32
 	if w.dtmfEvents != nil {
 		rtpTs = w.getTimestamp() // TODO: Maybe time to introduce the auto timestamp feature?
 	}
-	return dtmf.Write(w.ctx, w.dtmfAudio, w.dtmfEvents, rtpTs, string([]byte{digit}))
+	err := dtmf.Write(w.ctx, w.dtmfAudio, w.dtmfEvents, rtpTs, digits)
+	if err != nil {
+		return err
+	}
+	return nil
 }
