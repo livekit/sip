71eb234eb20c9becec5a41ae0bca4255e8d35701
Chelsea Bradbury <chelsea.bradbury@livekit.io>
2026-09-15 08:59:29 -0700

    Remove assignment of ParticipantAttributes during roomId state set

--- pkg/sip/inbound.go
+++ pkg/sip/inbound.go
@@ -1026,7 +1026,6 @@ func (c *inboundCall) handleInvite(ctx context.Context, tid traceid.ID, req *sip
 		if r := c.lkRoom.Room(); r != nil {
 			info.RoomId = r.SID()
 			info.RoomName = r.Name()
-			info.ParticipantAttributes = r.LocalParticipant.Attributes() // clones
 		} else {
 			c.log().Warnw("could not set RoomId: room is nil", nil)
 		}
