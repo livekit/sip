d21ec9ec98f6663f86a0d93734cd63c15b402f19
Chelsea Bradbury <chelsea.bradbury@livekit.io>
2026-09-21 16:47:41 -0700

    Defer state update before OnSessionEnd

--- pkg/sip/inbound.go
+++ pkg/sip/inbound.go
@@ -1640,7 +1640,7 @@ func (c *inboundCall) close(ctx context.Context, end EndCall) {
 	// Call the handler asynchronously to avoid blocking
 	if h := c.s.handler; h != nil {
 		if lastStatus := c.cc.lastCallStatus.Load(); lastStatus != nil {
-			c.state.Update(func(info *livekit.SIPCallInfo) {
+			c.state.DeferUpdate(func(info *livekit.SIPCallInfo) {
 				info.CallStatusCode = &livekit.SIPStatus{
 					Code:   lastStatus.Code,
 					Status: lastStatus.Status,
